33a64e02f654efc46ac561524228d1f0e240c1d6
NastyaKomar <yukarimoore@icloud.com>
2022-07-30 10:30:29 +0300

    feat: add the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	if (n < 0) {
+		n * -1;
+	} else {
+		n = n;
+	}
+	return parseInt(n.toString().split('').reverse().join(''));
 }
